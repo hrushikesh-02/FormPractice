4a510361b8fa934a402e2dd9faf00425d08d1b41
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-08-24 22:34:32 +0530

    reviewed error message and field components

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import TextError from "./TextError";
+
 const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
 };
 
 const onSubmit = (values) => {
@@ -28,19 +32,47 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field type="text" id="channel" name="channel" />
+          <Field
+            type="text"
+            id="channel"
+            name="channel"
+            placeholder="Youtube channel name"
+          />
           <ErrorMessage name="channel" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field type="text" id="address" name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log(props);
+              return (
+                <div className="form-control">
+                  <input id="address" {...field} />
+                  {meta.touched && meta.error && <div>{meta.error}</div>}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
         <button type="submit">Submit</button>
